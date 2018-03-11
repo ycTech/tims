d4b73b42459a79fcf28d735664ec9c515336c47a
刘志明 <liuzm@klxuexi.org>
2018-03-11 13:28:34 +0800

    上传集成

--- tims-manage/src/main/java/com/tims/manage/TimsManageController.java
+++ tims-manage/src/main/java/com/tims/manage/TimsManageController.java
@@ -1,9 +1,12 @@
 package com.tims.manage;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import com.tims.facade.config.TimsCoreHessianAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * manage启动类
@@ -11,8 +14,10 @@ import org.springframework.context.annotation.Import;
  * @author liuzm
  * @create 2018-02-27 17:56
  **/
+//解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
-@Import({TimsCoreHessianAutoConfiguration.class})
+@Import({TimsCoreHessianAutoConfiguration.class,FdfsClientConfig.class})
 public class TimsManageController {
     public static void main(String[] args) {
         SpringApplication.run(TimsManageController.class, args);
