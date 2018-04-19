82941b5c434856d66420919ff701859866003f58
liuzm <liuzm@klxuexi.org>
2018-04-19 21:25:57 +0800

    user mapper提交

---
+++ tims-core/src/main/java/com/tims/core/sys/mapper/SysUserInfoMapper.java
@@ -0,0 +1,35 @@
+package com.tims.core.sys.mapper;
+
+import com.tims.facade.sys.SysUserInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author liuzm
+ * @Description:
+ * @date 2018/4/1816:53
+ */
+@Mapper
+public interface SysUserInfoMapper {
+
+    /**
+     * 新增用户
+     * @param sysUserInfo
+     * @return
+     */
+    public  int saveSysUserInfo(@Param("sysUserInfo") SysUserInfo sysUserInfo);
+
+    /**
+     *更新用户
+     * @param sysUserInfo
+     * @return
+     */
+    public  int updateSysUserInfo(@Param("sysUserInfo") SysUserInfo sysUserInfo);
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    public  int deleteSysUserInfoById(@Param("id") String id);
+}
