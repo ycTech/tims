a5a2a84423561309e29f0da55f9a8e92b7b162d6
yecb@klxuexi.org <klxx123456>
2018-04-21 16:33:57 +0800

    前端代码提交

---
+++ tims-manage/src/main/resources/static/js/scanner-founder.js
@@ -0,0 +1,193 @@
+console.log('founder.js')
+GetScannerName()
+
+// {"code":200,"msg":"\u64cd\u4f5c\u6210\u529f","data":"http://192.168.1.195/group1/M00/00/00/wKgBw1rKJ-SAZZKoAAAEX-4DxsU348.jpg"}
+ScannerOcx = {
+  start: function () {
+    GetScannerName()
+  },
+  scan: function () {
+    console.log('founder scan')
+    Scan()
+  },
+  merge: function () {
+    SaveAsPDF()
+    console.log('founder merge')
+  },
+  upload: function () {
+    console.log('founder upload')
+  },
+  setting: function () {
+  }
+}
+function addEventListeners () {
+  if (window.attachEvent) {
+    document.getElementById('FScanX').attachEvent('OnRecvMsg', OnRecvMsg)
+  } else if (window.addEventListener) {
+    document.getElementById('FScanX').addEventListener('OnRecvMsg', OnRecvMsg, false)
+  }
+}
+function getFormData () {
+  var dataArray = $('#FounderScanner').serializeArray()
+  var formData = {}
+  for (var i = 0; i < dataArray.length; i++) {
+    formData[dataArray[i].name] = dataArray[i].value
+  }
+  console.log(JSON.stringify(formData))
+  return formData
+}
+function OnRecvMsg (msg) {
+  alert(msg)
+  console.log(JSON.stringify(msg))
+}
+function GetScannerName () {
+  var i = 0
+  var ScannerCount = window.FScanX.GetScannerCount()
+  var obj = document.getElementById('ScannerName')
+  obj.options.length = 0
+  for (i = 0; i < ScannerCount; i++) {
+    var ScannerName = window.FScanX.GetScannerName(i)
+    var objOption = document.createElement('OPTION')
+    objOption.text = ScannerName
+    objOption.value = i
+    obj.options.add(objOption)
+  }
+  SelScanner()
+}
+function SelScanner () {
+  var obj = document.getElementById('ScannerName')
+  var index = obj.selectedIndex
+  var ScannerName = obj.options[index].text
+  window.FScanX.SelScannerByName(ScannerName)
+}
+function ThumbWH () {
+  window.FScanX.ThumbWidth = document.getElementById('ThumbWidth').value
+  window.FScanX.ThumbHeight = document.getElementById('ThumbHeight').value
+}
+function Scan () {
+  window.FScanX.ScanShowUI = document.getElementById('ShowUI').value// 1为显示，0为不显示
+  window.FScanX.ShowThumb = document.getElementById('ShowThumb').value// 1为显示，0为不显示
+  window.FScanX.ScanSourceType = document.getElementById('SourceType').value// 0为平板，1为ADF正面，2为ADF双面
+  window.FScanX.ScanPixelType = document.getElementById('PixelType').value// 0为黑白，1为灰度，2为彩色
+  window.FScanX.ScanResolution = document.getElementById('Resolution').value// 分辨率
+  window.FScanX.Brightness = document.getElementById('Brightness').value// 亮度
+  window.FScanX.Contrast = document.getElementById('Contrast').value// 对比度
+  var FilePath = document.getElementById('FilePath').value// 图像保存路径
+  var FilePrefix = document.getElementById('FilePrefix').value// 图像名称前缀
+  var StartIndex = document.getElementById('StartIndex').value// 图像名称起始编号
+  var IndexLength = document.getElementById('IndexLength').value// 图像编号长度
+  window.FScanX.SetImageName(FilePath, FilePrefix, StartIndex, IndexLength)
+  window.FScanX.ImageFormat = document.getElementById('ImageFormat').value
+  window.FScanX.CompressionRate = document.getElementById('CompressionRate').value
+  window.FScanX.TiffCompressType = document.getElementById('TiffCompressType').value
+  window.FScanX.PDFCompressType = document.getElementById('PDFCompressType').value
+  window.FScanX.Scan()
+  var ScanImageCount = window.FScanX.GetScanImageCount()
+  document.getElementById('ScanImageCount').value = ScanImageCount
+  document.getElementById('StartIndex').value = parseInt(StartIndex) + ScanImageCount
+}
+function ScanAdd () { // 添加扫描,添加到最后一页
+  window.FScanX.ScanShowUI = document.getElementById('ShowUI').value// 1为显示，0为不显示
+  window.FScanX.ShowThumb = document.getElementById('ShowThumb').value// 1为显示，0为不显示
+  window.FScanX.ScanSourceType = document.getElementById('SourceType').value// 0为平板，1为ADF正面，2为ADF双面
+  window.FScanX.ScanPixelType = document.getElementById('PixelType').value// 0为黑白，1为灰度，2为彩色
+  window.FScanX.ScanResolution = document.getElementById('Resolution').value// 分辨率
+  window.FScanX.Brightness = document.getElementById('Brightness').value// 亮度
+  window.FScanX.Contrast = document.getElementById('Contrast').value// 对比度
+  window.FScanX.ScanAdd()
+}
+function ScanIns () { // 插入扫描，添加到选中页的前面
+  window.FScanX.ScanShowUI = document.getElementById('ShowUI').value// 1为显示，0为不显示
+  window.FScanX.ShowThumb = document.getElementById('ShowThumb').value// 1为显示，0为不显示
+  window.FScanX.ScanSourceType = document.getElementById('SourceType').value// 0为平板，1为ADF正面，2为ADF双面
+  window.FScanX.ScanPixelType = document.getElementById('PixelType').value// 0为黑白，1为灰度，2为彩色
+  window.FScanX.ScanResolution = document.getElementById('Resolution').value// 分辨率
+  window.FScanX.Brightness = document.getElementById('Brightness').value// 亮度
+  window.FScanX.Contrast = document.getElementById('Contrast').value// 对比度
+  window.FScanX.ScanIns()
+}
+function SaveAsPDF () {
+  window.FScanX.SaveAsPDF('D:\\test.pdf')
+}
+function DelOneImg () {
+  window.FScanX.DelOneImg()
+}
+function DelAllImg () {
+  window.FScanX.DelAllImg()
+}
+function ShowUISettings () {
+  window.FScanX.ShowUISettings()
+}
+function GetScanImagePath () {
+  var ImageIndex = document.getElementById('ImageIndex').value
+  document.getElementById('ScanImagePath').value = window.FScanX.GetScanImagePath(ImageIndex)
+}
+function GetBar () {
+  var BarCount = window.FScanX.GetBarCount(1)// 参数为要识别的是第几页
+  var BarData = ''
+  for (var i = 0; i < BarCount; i++) {
+    BarData = BarData + window.FScanX.GetBarData(i) + '\n'
+  }
+  document.getElementById('BarData').value = BarData
+}
+function uploadPdf () {
+  var base64 = window.FScanX.HttpSendFileEx("D:\\test.pdf", "http:\\localhost:8080");
+}
+function GetImageBase64 () {
+  var base64 = window.FScanX.GetImageBase64String(document.getElementById('ScanImagePath').value)
+  document.getElementById('ImageBase64').value = base64
+  console.log('upload base64')
+  // $.post('/scanner/api/fast/base64/upload', {
+  //   billNo: 'billNo',
+  //   billTypeId: 'billTypeId',
+  //   classifyId: 'classifyId',
+  //   classifyName: 'classifyName',
+  //   imageBase64: 'base64',
+  //   imageName: 'test.jpg'
+  // }, function (resp) {
+  //   console.log('resp success')
+  //   console.log(JSON.stringify(resp))
+  // }).done(function () {
+  //   console.log('second success')
+  // }).fail(function (error) {
+  //   console.log('error')
+  //   console.log(JSON.stringify(error))
+  // }).always(function () {
+  //   console.log('finished')
+  // })
+  var postData = {
+    billNo: 'billNo',
+    billTypeId: 'billTypeId',
+    classifyId: 'classifyId',
+    classifyName: 'classifyName',
+    imageBase64: base64,
+    imageName: 'test.jpg'
+  }
+  console.log(new Date().getTime())
+  $.ajax({
+    type: 'POST',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    traditional: true,
+    processData: false,
+    url: '/scanner/api/fast/base64/upload',
+    data: JSON.stringify(postData),
+    success: function (resp) {
+      console.log('resp success')
+      console.log(new Date().getTime())
+      console.log(JSON.stringify(resp))
+    },
+    error: function (error) {
+      console.log('error')
+      console.log(JSON.stringify(error))
+    }
+  })
+}
+function GetSN () {
+  var strSN = window.FScanX.GetSN('154F', '3202')
+  alert(strSN)
+}
+function HttpSendFileEx () {
+  var strrep = window.FScanX.HttpSendFileEx('D:\\test.pdf', 'http:\\localhost:8080')
+  alert(strrep)
+}
---
+++ tims-manage/src/main/resources/static/js/scanner-index.js
@@ -0,0 +1,301 @@
+var ScannerTypeMap = {
+  czur: 'scanner-czur.html',
+  founder: 'scanner-founder.html',
+  zhelin: 'scanner-zhelin.html'
+}
+var urlQuery = {
+  vsystem: getQueryString('vsystem'),
+  pk_corp: getQueryString('pk_corp'),
+  userCode: getQueryString('userCode'),
+  billType: getQueryString('billType'),
+  billtypename: getQueryString('billtypename'),
+  billId: getQueryString('billId'),
+  billNo: getQueryString('billNo'),
+  isFolder: getQueryString('isFolder'),
+  path: getQueryString('path')
+}
+console.log(JSON.stringify(urlQuery))
+var ScannerOcx = {
+  start: function () {
+    alert('请选择设备并启动该设备，请确保已正确安装驱动！')
+  },
+  scan: function () {
+    alert('请选择设备并启动该设备，请确保已正确安装驱动！')
+  },
+  merge: function () {
+    alert('请选择设备并启动该设备，请确保已正确安装驱动！')
+  },
+  upload: function () {
+    alert('请选择设备并启动该设备，请确保已正确安装驱动！')
+  },
+  setting: function () {
+    alert('请选择设备并启动该设备，请确保已正确安装驱动！')
+  }
+}
+
+ScannerHome = {
+  params: urlQuery,
+  // 上传Base64格式图片预览
+  uploadImageBase64Preview: function (base64, fileName, fileLocalPath, cb) {
+    var postData = {
+      imageBase64: base64,
+      imageName: fileName
+    }
+    $.ajax({
+      url: baseUrl + 'fast/group/base64/upload',
+      method: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      traditional: true,
+      processData: false,
+      async: false,
+      data: JSON.stringify(postData),
+      success: function (resp) {
+        var res = resp.data
+        var fileUrl = res.fileUrl
+        var thumbImageUrl = res.thumbImageUrl
+        AddImagePreview(fileUrl, thumbImageUrl)
+        cb(fileUrl, thumbImageUrl)
+      },
+      error: function (error) {
+        console.log('fast/group/base64/upload error')
+        console.log(JSON.stringify(error))
+      }
+    })
+  },
+  uploadImageFilePreview: function () {
+  },
+  uploadPdfBase64: function (fileName, base64, callback) {
+    var postData = {
+      vsystem: getQueryString('vsystem'),
+      pk_corp: getQueryString('pk_corp'),
+      userCode: getQueryString('userCode'),
+      billType: getQueryString('billType'),
+      billtypename: getQueryString('billtypename'),
+      billId: getQueryString('billId'),
+      billNo: getQueryString('billNo'),
+      isFolder: getQueryString('isFolder'),
+      path: getQueryString('path'),
+      imageBase64: base64,
+      imageName: fileName
+    }
+    $.ajax({
+      url: baseUrl + 'fast/base64/upload',
+      method: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      traditional: true,
+      processData: false,
+      async: false,
+      data: JSON.stringify(postData),
+      success: function (resp) {
+        var fileUrl = resp.data
+        console.log(JSON.stringify(resp))
+        callback(fileUrl)
+      },
+      error: function (error) {
+        console.log('fast/group/base64/upload error')
+        console.log(JSON.stringify(error))
+      }
+    })
+  },
+  uploadPdfFile: function () {
+  },
+  uploadImageBase64: function () {
+  },
+  uploadImageFile: function () {
+  }
+}
+
+$(function () {
+  initEventListening()
+  // 初始化
+  initLayout()
+  initJsTree()
+  function initLayout () {
+    // 初始化工具栏按钮提示框
+    $('[data-toggle="tooltip"]').tooltip()
+
+    // 加载默认设备对应的OCX控件及配置界面
+    var type = $('#device-select').children('option:selected').val()
+    loadHtml(ScannerTypeMap[type])
+  }
+
+  // 初始化事件监听
+  function initEventListening () {
+    onDeviceTypeChange()
+    onActionButtonClick()
+  }
+
+  function onActionButtonClick () {
+    // 启动设备
+    $('#action-button__start').click(function (e) {
+      e.preventDefault()
+      ScannerOcx.start()
+    })
+
+    // 开始扫描
+    $('#action-button__scan').click(function (e) {
+      e.preventDefault()
+      ScannerOcx.scan()
+    })
+
+    // 合并PDF
+    $('#action-button__merge').click(function (e) {
+      e.preventDefault()
+      ScannerOcx.merge()
+    })
+
+    // 上传PDF
+    $('#action-button__upload').click(function (e) {
+      e.preventDefault()
+      ScannerOcx.upload()
+    })
+
+    // 设置
+    $('#action-button__setting').click(function (e) {
+      e.preventDefault()
+      toggleScannerConfig()
+      ScannerOcx.setting()
+    })
+  }
+})
+
+// 初始化JsTree
+function initJsTree () {
+  $.ajax({
+    type: 'POST',
+    url: baseUrl + 'bill/file/list',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: JSON.stringify(urlQuery),
+    success: function (res) {
+      var treeData = res.data.file
+      var fileArray = res.data.fileStoreList
+      $('#jstree').jstree({
+        core: {
+          data: parseTreeData(treeData)
+        }
+      })
+    },
+    error: function (error) {
+      console.log(JSON.stringify(error))
+    }
+  })
+  function parseTreeData (treeArray) {
+    for (var i = 0; i < treeArray.length; i++) {
+      var item = treeArray[i]
+      item.text = item.name
+      item.state = {
+        opened: true
+      }
+      if (item.children && item.children.length > 0) {
+        item.icon = 'glyphicon glyphicon-folder-open'
+        item.children = parseTreeData(item.children)
+      } else {
+        item.icon = 'glyphicon glyphicon-file'
+        item.li_attr = {
+          fileUrl: item.url
+        }
+      }
+    }
+    return treeArray
+  }
+}
+
+// 关闭配置面板
+function onCloseConfig () {
+  showImagePreview()
+}
+
+// 切换设备配置面板
+function toggleScannerConfig () {
+  $('.image-preview__wrapper,.scanner-setting__wrapper').toggle()
+}
+// 显示图片预览
+function showImagePreview () {
+  $('.image-preview__wrapper').show()
+  $('.scanner-setting__wrapper').hide()
+}
+// 显示扫描设备配置项
+function showScannerConfig () {
+  $('.image-preview__wrapper').hide()
+  $('.scanner-setting__wrapper').show()
+}
+
+// 监听设备类型改变事件
+function onDeviceTypeChange () {
+  $('#device-select').change(function (e) {
+    e.preventDefault()
+    var type = $('#device-select').children('option:selected').val()
+    loadHtml(ScannerTypeMap[type])
+  })
+}
+
+// 加载不同扫描设备的面板
+function loadHtml (htmlPath) {
+  $.get(htmlPath, function (data) {
+    $('#scanner-iframe').empty().html(data)
+    showImagePreview()
+    toggleScannerConfig()
+  })
+}
+function AddImagePreview (fileUrl, thumbImageUrl) {
+  var $imageThumb = $('<img/>').attr({
+    src: thumbImageUrl,
+    'origin-src': fileUrl
+  }).addClass('image-thumbnail').bind('click', function () {
+    var $imgThumb = $(this)
+    var src = $imgThumb.attr('origin-src')
+    var contentW = $('.image-preview__wrapper').width()
+    var contentH = $('.image-preview__wrapper').height()
+    var imgW = $imgThumb.attr('img-w')
+    var imgH = $imgThumb.attr('img-h')
+
+    var $imagePreview = $('<img/>')
+      .attr('src', src)
+      .addClass('image-preview')
+
+    if (imgW / imgH > contentW / contentH) {
+      $imagePreview.css({
+        width: '100%',
+        height: 'auto'
+      })
+    } else {
+      $imagePreview.css({
+        width: 'auto',
+        height: '100%'
+      })
+    }
+
+    var $imagePreviewContent = $('<div/>')
+      .addClass('image-preview-content')
+      .append($imagePreview)
+
+    $('.image-preview__wrapper')
+      .empty()
+      .append($imagePreviewContent)
+
+    showImagePreview()
+  })
+  var $imageThumbWrapper = $('<div/>')
+    .addClass('image-thumbnail-wrapper')
+    .append($imageThumb)
+  $('.image-thumbnails')
+    .append($imageThumbWrapper)
+  setTimeout(function () {
+    var imageThumbW = $imageThumb.width()
+    var imageThumbH = $imageThumb.height()
+    $imageThumb.attr({
+      'img-w': imageThumbW,
+      'img-h': imageThumbH
+    })
+    if (imageThumbH > imageThumbW) {
+      $imageThumb.css('height', '100%')
+      $imageThumb.css('width', 'auto')
+    } else {
+      $imageThumb.css('height', 'auto')
+      $imageThumb.css('width', '100%')
+    }
+  }, 0)
+}
