0c3b427da73ea8f215d6b7cb5472a46ae4c0b324
yecb@klxuexi.org <klxx123456>
2018-04-24 12:35:29 +0800

    前端代码提交

--- tims-manage/src/main/java/com/tims/manage/test/FileClient.java
+++ tims-manage/src/main/java/com/tims/manage/test/FileClient.java
@@ -2,25 +2,31 @@ package com.tims.manage.test;
 
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class FileClient {
+    public  static  final  String  CHARSET  =  "UTF-8";    
     /** 
           * 模拟表单上传文件 
           */  
@@ -78,22 +84,107 @@ public class FileClient {
         return resultMap;    
      }
 
-    public  static  void  main(String[]  args)  {
-        FileClient  httpsUtils  =  new  FileClient();
-        //要上传的文件的路径
-        String  filePath  =  "c:/1/test5.doc";
-        String  postUrl    =  "http://hy.nfox.ml:10060/smfile/upload";
-        Map<String,String>  postParam  =  new  HashMap<String,String>();
-        postParam.put("userCode",  "11122");
-        postParam.put("billType",  "11122");
-        postParam.put("billId",  "11122");
-        postParam.put("billNo",  "11122");
-        postParam.put("path",  "11122");
-        postParam.put("isFolder",  "11122");
+    /** 
+      * HTTP Post 获取内容
+      * @return 页面内容 
+      * @throws IOException  
+      * @throws ClientProtocolException  
+      */  
+              
+    public static String sendPost(String url, String params) throws ClientProtocolException, IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        File  postFile  =  new  File(filePath);
-        Map<String,Object>  resultMap  =  httpsUtils.uploadFileByHTTP(postFile,postUrl,postParam);
-        System.out.println(resultMap);
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-type","application/json; charset=utf-8");  
+        httpPost.setHeader("Accept", "application/json");
+
+        StringEntity entity = new StringEntity(params, CHARSET);
+        httpPost.setEntity(entity);     
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        int statusCode = response.getStatusLine().getStatusCode();  
+        if (statusCode != 200) {  
+            httpPost.abort();  
+            throw new RuntimeException("HttpClient,error status code :" + statusCode);  
+        }  
+        HttpEntity responseEntity = response.getEntity();
+        String result = null;  
+        if (responseEntity != null) {
+            result = EntityUtils.toString(responseEntity, "utf-8");
+            EntityUtils.consume(entity);  
+            response.close();  
+            return result;  
+        }else{  
+             return null;  
+        }  
+    }  
+
+/** 
+      * HTTP Get 获取内容
+      * @return 页面内容 
+      */
+    public static String sendGet(String url, Map<String, Object> params) throws ParseException, UnsupportedEncodingException, IOException{
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        if(params !=null && !params.isEmpty()){  
+              
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>(params.size());  
+              
+            for (String key :params.keySet()){  
+                pairs.add(new BasicNameValuePair(key, params.get(key).toString()));  
+            }  
+            url +="?"+EntityUtils.toString(new UrlEncodedFormEntity(pairs), CHARSET);  
+        }  
+          
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        int statusCode = response.getStatusLine().getStatusCode();  
+        if(statusCode !=200){  
+            httpGet.abort();  
+            throw new RuntimeException("HttpClient,error status code :" + statusCode);  
+        }  
+        HttpEntity entity = response.getEntity();  
+        String result = null;  
+        if (entity != null) {  
+            result = EntityUtils.toString(entity, "utf-8");  
+            EntityUtils.consume(entity);  
+            response.close();  
+            return result;  
+        }else{  
+            return null;  
+        }  
+    }  
+
+    public  static  void  main(String[]  args) throws IOException {
+//        FileClient  httpsUtils  =  new  FileClient();
+//        //要上传的文件的路径
+//        String  filePath  =  "c:/1/test5.doc";
+//        String  postUrl    =  "http://hy.nfox.ml:10060/smfile/upload";
+//        Map<String,String>  postParam  =  new  HashMap<String,String>();
+//        postParam.put("userCode",  "11122");
+//        postParam.put("billType",  "11122");
+//        postParam.put("billId",  "11122");
+//        postParam.put("billNo",  "11122");
+//        postParam.put("path",  "11122");
+//        postParam.put("isFolder",  "11122");
+//
+//        File  postFile  =  new  File(filePath);
+//        Map<String,Object>  resultMap  =  httpsUtils.uploadFileByHTTP(postFile,postUrl,postParam);
+//        System.out.println(resultMap);
+        /**   POST请求   **/
+        String testUrl02 = "http://hy.nfox.ml:10060/smfile/save/user";
+//        Map<String, Object> params02 = new HashMap<>();
+//        params02.put("userCode", "001");
+//        params02.put("userName", "测试用户");
+        String params="{\n" +
+                "  \"userCode\": \"002\",\n" +
+                "  \"userName\": \"test\"\n" +
+                "}";
+        System.out.println(sendPost(testUrl02,params));
+        /**   GET请求   **/
+//        String testUrl01 = "具体的测试接口地址";
+//        Map<String, Object> params01 = new HashMap<>();
+//        params01.put("参数01", "对应的参数取值");
+//        params01.put("参数02", "");
+//        System.out.println(sendGet(testUrl01, params01));
     }
 
 }
